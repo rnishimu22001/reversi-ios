3b98dfedbb43dec12ba0f8274cb6cb3966c0a866
rnishimu22001 <rnishimu22001@yahoo.co.jp>
2020-04-24 20:40:18 +0900

    Mock追加

---
+++ ReversiTests/Mock/MockBoardView.swift
@@ -0,0 +1,29 @@
+//
+//  MockBoardView.swift
+//  ReversiTests
+//
+//  Created by rnishimu on 2020/04/24.
+//  Copyright © 2020 Yuta Koshizawa. All rights reserved.
+//
+
+@testable import Reversi
+
+final class MockBoardView: BoardView {
+    
+    var setDiskCompletion: (((disk: Disk?, x: Int, y: Int, animated: Bool)) -> Void)?
+    var isSuccess: Bool = true
+    override func setDisk(_ disk: Disk?, atX x: Int, y: Int, animated: Bool, completion: ((Bool) -> Void)? = nil) {
+        setDiskCompletion?((disk: disk, x: x, y: y, animated))
+        completion?(isSuccess)
+    }
+    
+    var resetCompletion: (() -> Void)?
+    override func reset() {
+        resetCompletion?()
+    }
+    
+    var dummyDisk: Disk?
+    override func diskAt(x: Int, y: Int) -> Disk? {
+        return dummyDisk
+    }
+}
