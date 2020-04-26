02d858833c08ce96550efa7f7e336a92a9608fd0
rnishimu22001 <rnishimu22001@yahoo.co.jp>
2020-04-26 23:37:28 +0900

    specifications追加

---
+++ Reversi/ReversiSpecifications.swift
@@ -0,0 +1,11 @@
+//
+//  ReversiSpecifications.swift
+//  Reversi
+//
+//  Created by rnishimu on 2020/04/26.
+//  Copyright © 2020 Yuta Koshizawa. All rights reserved.
+//
+
+struct ReversiSpecifications {
+    
+}
