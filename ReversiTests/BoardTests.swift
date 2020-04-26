8a4e247b5a6df9f99761f8b8cba35a9d3aff4c0f
rnishimu22001 <rnishimu22001@yahoo.co.jp>
2020-04-26 20:03:23 +0900

    テスト追加

--- ReversiTests/BoardTests.swift
+++ ReversiTests/BoardTests.swift
@@ -15,4 +15,49 @@ final class BoardTests: XCTestCase {
         XCTAssertEqual(Board().xRange, BoardView().xRange)
         XCTAssertEqual(Board().yRange, BoardView().yRange)
     }
+    
+    func testSet() {
+        XCTContext.runActivity(named: "セット成功") { _ in
+            // Given
+            var target = Board()
+            let coordinates = Coordinates(x: 0, y: 0)
+            // When
+            do {
+                try target.set(disk: .dark, atX: coordinates.x, y: coordinates.y)
+            } catch {
+                XCTFail("座標がボードの範囲内のため成功する")
+            }
+            // Then
+            XCTAssertEqual(target.disks[coordinates], .dark)
+        }
+        XCTContext.runActivity(named: "セット失敗 - y軸が範囲外") { _ in
+            // Given
+            var target = Board()
+            let coordinates = Coordinates(x: 0, y: 8)
+            // When
+            do {
+                try target.set(disk: .dark, atX: coordinates.x, y: coordinates.y)
+                XCTFail("座標がボードの範囲外のため失敗する")
+            } catch (let error) {
+                guard case BoardError.outOfRange = error else {
+                    XCTFail("範囲外のエラーではない")
+                    return
+                }
+            }
+        }
+        XCTContext.runActivity(named: "セット失敗 - x軸が範囲外") { _ in
+            // Given
+            var target = Board()
+            let coordinates = Coordinates(x: 8, y: 0)
+            // When
+            do {
+                try target.set(disk: .dark, atX: coordinates.x, y: coordinates.y)
+            } catch (let error) {
+                guard case BoardError.outOfRange = error else {
+                    XCTFail("範囲外のエラーではない")
+                    return
+                }
+            }
+        }
+    }
 }
